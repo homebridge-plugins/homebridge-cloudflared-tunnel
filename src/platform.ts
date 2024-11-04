e66fe93b56ca10fbeebca4e596a5cb953f3b102c
Donavan Becker <beckersmarthome@icloud.com>
2024-11-03 23:54:45 -0600

    v1.1.1 (#13)
    ## [1.1.1](https://github.com/homebridge-plugins/homebridge-cloudflared-tunnel/releases/tag/v1.1.1) (2024-11-03)
    
    ### What's Changes
    
    - Housekeeping and updated dependencies.
    
    **Full Changelog**: https://github.com/homebridge-plugins/homebridge-cloudflared-tunnel/compare/v1.1.0...v1.1.1

--- src/platform.ts
+++ src/platform.ts
@@ -2,13 +2,17 @@
  *
  * platform.ts: homebridge-cloudflared-tunnel.
  */
-import { startTunnel } from 'untun';
-import { readFileSync } from 'fs';
-import { CloudflaredTunnel } from './cloudflared-tunnel.js';
+import type { API, DynamicPlatformPlugin, HAP, Logging, PlatformAccessory } from 'homebridge'
+import type { TunnelOptions } from 'untun'
 
-import type { TunnelOptions } from 'untun';
-import type { CloudflaredTunnelPlatformConfig } from './settings.js';
-import type { API, DynamicPlatformPlugin, HAP, Logging, PlatformAccessory } from 'homebridge';
+import type { CloudflaredTunnelPlatformConfig } from './settings.js'
+
+import { readFileSync } from 'node:fs'
+import { argv } from 'node:process'
+
+import { startTunnel } from 'untun'
+
+import { CloudflaredTunnel } from './cloudflared-tunnel.js'
 
 /**
  * HomebridgePlatform
@@ -16,36 +20,40 @@ import type { API, DynamicPlatformPlugin, HAP, Logging, PlatformAccessory } from
  * parse the user config and discover/register accessories with Homebridge.
  */
 export class CloudflaredTunnelPlatform implements DynamicPlatformPlugin {
-  public accessories: PlatformAccessory[];
-  public readonly api: API;
-  public readonly log: Logging;
-  protected readonly hap: HAP;
-  public config!: CloudflaredTunnelPlatformConfig;
+  public accessories: PlatformAccessory[]
+  public readonly api: API
+  public readonly log: Logging
+  protected readonly hap: HAP
+  public config!: CloudflaredTunnelPlatformConfig
 
-  platformConfig!: CloudflaredTunnelPlatformConfig;
-  platformLogging!: CloudflaredTunnelPlatformConfig['logging'];
-  debugMode!: boolean;
+  platformConfig!: CloudflaredTunnelPlatformConfig
+  platformLogging!: CloudflaredTunnelPlatformConfig['logging']
+  platformRefreshRate: CloudflaredTunnelPlatformConfig['refreshRate']
+  platformUpdateRate: CloudflaredTunnelPlatformConfig['updateRate']
+  platformPushRate: CloudflaredTunnelPlatformConfig['pushRate']
+  debugMode!: boolean
+  version!: string
 
   constructor(
     log: Logging,
     config: CloudflaredTunnelPlatformConfig,
     api: API,
   ) {
-    this.accessories = [];
-    this.api = api;
-    this.hap = this.api.hap;
-    this.log = log;
+    this.accessories = []
+    this.api = api
+    this.hap = this.api.hap
+    this.log = log
     // only load if configured
     if (!config) {
-      return;
+      return
     }
 
     // Plugin options into our config variables.
     this.config = {
       platform: 'CloudflaredTunnel',
+      name: config.name,
       domain: config.domain as string,
       token: config.token as string,
-      name: config.name as string,
       url: config.url as string,
       port: config.port as number,
       hostname: config.hostname as string,
@@ -53,41 +61,46 @@ export class CloudflaredTunnelPlatform implements DynamicPlatformPlugin {
       verifyTLS: config.verifyTLS as boolean,
       logging: config.logging as string,
       acceptCloudflareNotice: config.acceptCloudflareNotice as boolean,
-    };
-    this.platformConfigOptions();
-    this.platformLogs();
+    }
+
+    // Plugin Configuration
+    this.getPlatformLogSettings()
+    this.getPlatformRateSettings()
+    this.getPlatformConfigSettings()
+    this.getVersion()
+
+    // Finish initializing the platform
     this.debugLog(`Finished initializing platform: ${config.name}`);
 
     // verify the config
     (async () => {
       try {
-        await this.verifyConfig();
-        await this.debugLog('Config OK');
+        await this.verifyConfig()
+        await this.debugLog('Config OK')
       } catch (e: any) {
-        this.errorLog(`Verify Config, Error Message: ${e.message}, Submit Bugs Here: https://bit.ly/homebridge-cloudflared-tunnel-bug-report`);
-        this.debugErrorLog(`Verify Config, Error: ${e}`);
-        return;
+        this.errorLog(`Verify Config, Error Message: ${e.message}, Submit Bugs Here: https://bit.ly/homebridge-cloudflared-tunnel-bug-report`)
+        this.debugErrorLog(`Verify Config, Error: ${e}`)
       }
-    })();
+    })()
 
     // When this event is fired it means Homebridge has restored all cached accessories from disk.
     // Dynamic Platform plugins should only register new accessories after this event was fired,
     // in order to ensure they weren't added to homebridge already. This event can also be used
     // to start discovery of new accessories.
     this.api.on('didFinishLaunching', async () => {
-      log.debug('Executed didFinishLaunching callback');
+      log.debug('Executed didFinishLaunching callback')
       // run the method to discover / register your devices as accessories
       try {
         if (this.config.domain) {
-          await this.existingTunnel();
+          await this.existingTunnel()
         } else {
-          await this.createTunnel();
+          await this.createTunnel()
         }
       } catch (e: any) {
-        this.errorLog(`Failed to Start Tunnel, Error Message: ${JSON.stringify(e.message)}`);
-        this.debugErrorLog(JSON.stringify(e));
+        this.errorLog(`Failed to Start Tunnel, Error Message: ${JSON.stringify(e.message)}`)
+        this.debugErrorLog(JSON.stringify(e))
       }
-    });
+    })
   }
 
   /**
@@ -95,10 +108,10 @@ export class CloudflaredTunnelPlatform implements DynamicPlatformPlugin {
    * It should be used to setup event handlers for characteristics and update respective values.
    */
   configureAccessory(accessory: PlatformAccessory) {
-    //this.infoLog(`Loading accessory from cache: ${accessory.displayName}`);
+    // this.infoLog(`Loading accessory from cache: ${accessory.displayName}`);
 
     // add the restored accessory to the accessories cache so we can track if it has already been registered
-    this.accessories.push(accessory);
+    this.accessories.push(accessory)
   }
 
   /**
@@ -106,35 +119,35 @@ export class CloudflaredTunnelPlatform implements DynamicPlatformPlugin {
    */
   async verifyConfig() {
     if (!this.config.url && (!this.config.protocol && !this.config.hostname && !this.config.port) && (!this.config.domain && !this.config.token)) {
-      throw new Error('Missing required config: url or {protocol}://{hostname}:{port}, please check your config.json');
+      throw new Error('Missing required config: url or {protocol}://{hostname}:{port}, please check your config.json')
     }
     if (this.config.url && this.config.hostname) {
-      throw new Error('Cannot have both url and hostname in config. Please remove one.');
+      throw new Error('Cannot have both url and hostname in config. Please remove one.')
     }
     if (this.config.domain && !this.config.token) {
-      throw new Error('Missing required config: token, please check your config.json');
+      throw new Error('Missing required config: token, please check your config.json')
     }
     if (this.config.token && !this.config.domain) {
-      this.warnLog('Missing config: domain, this is is needed to display in the logs which domain is being tunneled, please check your config.json');
+      this.warnLog('Missing config: domain, this is is needed to display in the logs which domain is being tunneled, please check your config.json')
     }
     if (!this.config.logging) {
-      this.config.logging = 'standard';
+      this.config.logging = 'standard'
     }
     if (!this.config.acceptCloudflareNotice) {
-      this.config.acceptCloudflareNotice = false;
+      this.config.acceptCloudflareNotice = false
     }
   }
 
   async existingTunnel() {
-    const tunnel = new CloudflaredTunnel();
-    tunnel.token = this.config.token;
-    await this.infoLog(`Starting Tunnel with Domain: ${this.config.domain}`);
-    tunnel.start();
+    const tunnel = new CloudflaredTunnel()
+    tunnel.token = this.config.token
+    await this.infoLog(`Starting Tunnel with Domain: ${this.config.domain}`)
+    tunnel.start()
   }
 
   async createTunnel() {
-    await this.debugLog(JSON.stringify(this.config));
-    //The local server URL to tunnel.
+    await this.debugLog(JSON.stringify(this.config))
+    // The local server URL to tunnel.
     const options: TunnelOptions = {
       url: this.config.url,
       protocol: this.config.protocol,
@@ -142,64 +155,105 @@ export class CloudflaredTunnelPlatform implements DynamicPlatformPlugin {
       port: this.config.port,
       verifyTLS: this.config.verifyTLS,
       acceptCloudflareNotice: this.config.acceptCloudflareNotice,
-    };
-    await this.debugWarnLog(`Starting Tunnel with Options: ${JSON.stringify(options)}`);
-    const autoTunnel = await startTunnel(options);
+    }
+    await this.debugWarnLog(`Starting Tunnel with Options: ${JSON.stringify(options)}`)
+    const autoTunnel = await startTunnel(options)
     if (autoTunnel) {
-      const tunnelURL = await autoTunnel.getURL();
-      await this.infoLog(`Tunnel URL: ${JSON.stringify(tunnelURL)}`);
+      const tunnelURL = await autoTunnel.getURL()
+      await this.infoLog(`Tunnel URL: ${JSON.stringify(tunnelURL)}`)
     }
   }
 
-  async platformConfigOptions() {
-    const platformConfig: CloudflaredTunnelPlatformConfig = {
-      platform: '',
-    };
-    if (this.config.logging) {
-      platformConfig.logging = this.config.logging;
+  async getPlatformConfigSettings() {
+    if (this.config.options) {
+      const platformConfig: CloudflaredTunnelPlatformConfig = {
+        platform: 'CloudflaredTunnel',
+      }
+      platformConfig.logging = this.config.options.logging ? this.config.options.logging : undefined
+      platformConfig.refreshRate = this.config.options.refreshRate ? this.config.options.refreshRate : undefined
+      platformConfig.updateRate = this.config.options.updateRate ? this.config.options.updateRate : undefined
+      platformConfig.pushRate = this.config.options.pushRate ? this.config.options.pushRate : undefined
+      if (Object.entries(platformConfig).length !== 0) {
+        await this.debugLog(`Platform Config: ${JSON.stringify(platformConfig)}`)
+      }
+      this.platformConfig = platformConfig
     }
-    if (this.config.refreshRate) {
-      platformConfig.refreshRate = this.config.refreshRate;
-    }
-    if (Object.entries(platformConfig).length !== 0) {
-      this.debugLog(`Platform Config: ${JSON.stringify(platformConfig)}`);
-    }
-    this.platformConfig = platformConfig;
   }
 
-  async platformLogs() {
-    this.debugMode = process.argv.includes('-D') || process.argv.includes('--debug');
-    this.platformLogging = this.config.options?.logging ?? 'standard';
-    if (this.config.options?.logging === 'debug' || this.config.options?.logging === 'standard' || this.config.options?.logging === 'none') {
-      this.platformLogging = this.config.options.logging;
-      if (await this.loggingIsDebug()) {
-        this.debugWarnLog(`Using Config Logging: ${this.platformLogging}`);
-      }
-    } else if (this.debugMode) {
-      this.platformLogging = 'debugMode';
-      if (await this.loggingIsDebug()) {
-        this.debugWarnLog(`Using ${this.platformLogging} Logging`);
-      }
+  async getPlatformRateSettings() {
+    this.platformRefreshRate = this.config.options?.refreshRate ? this.config.options.refreshRate : 0
+    const refreshRate = this.config.options?.refreshRate ? 'Using Platform Config refreshRate' : 'refreshRate Disabled by Default'
+    await this.debugLog(`${refreshRate}: ${this.platformRefreshRate}`)
+    this.platformUpdateRate = this.config.options?.updateRate ? this.config.options.updateRate : 1
+    const updateRate = this.config.options?.updateRate ? 'Using Platform Config updateRate' : 'Using Default updateRate'
+    await this.debugLog(`${updateRate}: ${this.platformUpdateRate}`)
+    this.platformPushRate = this.config.options?.pushRate ? this.config.options.pushRate : 1
+    const pushRate = this.config.options?.pushRate ? 'Using Platform Config pushRate' : 'Using Default pushRate'
+    await this.debugLog(`${pushRate}: ${this.platformPushRate}`)
+  }
+
+  async getPlatformLogSettings() {
+    this.debugMode = argv.includes('-D') ?? argv.includes('--debug')
+    this.platformLogging = (this.config.options?.logging === 'debug' || this.config.options?.logging === 'standard'
+      || this.config.options?.logging === 'none')
+      ? this.config.options.logging
+      : this.debugMode ? 'debugMode' : 'standard'
+    const logging = this.config.options?.logging ? 'Platform Config' : this.debugMode ? 'debugMode' : 'Default'
+    await this.debugLog(`Using ${logging} Logging: ${this.platformLogging}`)
+  }
+
+  /**
+   * Asynchronously retrieves the version of the plugin from the package.json file.
+   *
+   * This method reads the package.json file located in the parent directory,
+   * parses its content to extract the version, and logs the version using the debug logger.
+   * The extracted version is then assigned to the `version` property of the class.
+   *
+   * @returns {Promise<void>} A promise that resolves when the version has been retrieved and logged.
+   */
+  async getVersion(): Promise<void> {
+    const { version } = JSON.parse(readFileSync(new URL('../package.json', import.meta.url), 'utf-8'))
+    this.debugLog(`Plugin Version: ${version}`)
+    this.version = version
+  }
+
+  /**
+   * Validate and clean a string value for a Name Characteristic.
+   * @param displayName - The display name of the accessory.
+   * @param name - The name of the characteristic.
+   * @param value - The value to be validated and cleaned.
+   * @returns The cleaned string value.
+   */
+  async validateAndCleanDisplayName(displayName: string, name: string, value: string): Promise<string> {
+    if (this.config.options?.allowInvalidCharacters) {
+      return value
     } else {
-      this.platformLogging = 'standard';
-      if (await this.loggingIsDebug()) {
-        this.debugWarnLog(`Using ${this.platformLogging} Logging`);
-      }
-    }
-    if (this.debugMode) {
-      this.platformLogging = 'debugMode';
-    }
-  }
+      const validPattern = /^[\p{L}\p{N}][\p{L}\p{N} ']*[\p{L}\p{N}]$/u
+      const invalidCharsPattern = /[^\p{L}\p{N} ']/gu
+      const invalidStartEndPattern = /^[^\p{L}\p{N}]+|[^\p{L}\p{N}]+$/gu
 
-  async getVersion() {
-    const json = JSON.parse(
-      readFileSync(
-        new URL('../package.json', import.meta.url),
-        'utf-8',
-      ),
-    );
-    this.debugLog(`Plugin Version: ${json.version}`);
-    return json.version;
+      if (typeof value === 'string' && !validPattern.test(value)) {
+        this.warnLog(`WARNING: The accessory '${displayName}' has an invalid '${name}' characteristic ('${value}'). Please use only alphanumeric, space, and apostrophe characters. Ensure it starts and ends with an alphabetic or numeric character, and avoid emojis. This may prevent the accessory from being added in the Home App or cause unresponsiveness.`)
+
+        // Remove invalid characters
+        if (invalidCharsPattern.test(value)) {
+          const before = value
+          this.warnLog(`Removing invalid characters from '${name}' characteristic, if you feel this is incorrect,  please enable \'allowInvalidCharacter\' in the config to allow all characters`)
+          value = value.replace(invalidCharsPattern, '')
+          this.warnLog(`${name} Before: '${before}' After: '${value}'`)
+        }
+
+        // Ensure it starts and ends with an alphanumeric character
+        if (invalidStartEndPattern.test(value)) {
+          const before = value
+          this.warnLog(`Removing invalid starting or ending characters from '${name}' characteristic, if you feel this is incorrect, please enable \'allowInvalidCharacter\' in the config to allow all characters`)
+          value = value.replace(invalidStartEndPattern, '')
+          this.warnLog(`${name} Before: '${before}' After: '${value}'`)
+        }
+      }
+
+      return value
+    }
   }
 
   /**
@@ -208,48 +262,48 @@ export class CloudflaredTunnelPlatform implements DynamicPlatformPlugin {
    */
   async infoLog(...log: any[]): Promise<void> {
     if (await this.enablingPlatformLogging()) {
-      this.log.info(String(...log));
+      this.log.info(String(...log))
     }
   }
 
   async successLog(...log: any[]): Promise<void> {
     if (await this.enablingPlatformLogging()) {
-      this.log.success(String(...log));
+      this.log.success(String(...log))
     }
   }
 
   async debugSuccessLog(...log: any[]): Promise<void> {
     if (await this.enablingPlatformLogging()) {
       if (await this.loggingIsDebug()) {
-        this.log.success('[DEBUG]', String(...log));
+        this.log.success('[DEBUG]', String(...log))
       }
     }
   }
 
   async warnLog(...log: any[]): Promise<void> {
     if (await this.enablingPlatformLogging()) {
-      this.log.warn(String(...log));
+      this.log.warn(String(...log))
     }
   }
 
   async debugWarnLog(...log: any[]): Promise<void> {
     if (await this.enablingPlatformLogging()) {
       if (await this.loggingIsDebug()) {
-        this.log.warn('[DEBUG]', String(...log));
+        this.log.warn('[DEBUG]', String(...log))
       }
     }
   }
 
   async errorLog(...log: any[]): Promise<void> {
     if (await this.enablingPlatformLogging()) {
-      this.log.error(String(...log));
+      this.log.error(String(...log))
     }
   }
 
   async debugErrorLog(...log: any[]): Promise<void> {
     if (await this.enablingPlatformLogging()) {
       if (await this.loggingIsDebug()) {
-        this.log.error('[DEBUG]', String(...log));
+        this.log.error('[DEBUG]', String(...log))
       }
     }
   }
@@ -257,18 +311,18 @@ export class CloudflaredTunnelPlatform implements DynamicPlatformPlugin {
   async debugLog(...log: any[]): Promise<void> {
     if (await this.enablingPlatformLogging()) {
       if (this.platformLogging === 'debugMode') {
-        this.log.debug(String(...log));
+        this.log.debug(String(...log))
       } else if (this.platformLogging === 'debug') {
-        this.log.info('[DEBUG]', String(...log));
+        this.log.info('[DEBUG]', String(...log))
       }
     }
   }
 
   async loggingIsDebug(): Promise<boolean> {
-    return this.platformLogging === 'debugMode' || this.platformLogging === 'debug';
+    return this.platformLogging === 'debugMode' || this.platformLogging === 'debug'
   }
 
   async enablingPlatformLogging(): Promise<boolean> {
-    return this.platformLogging === 'debugMode' || this.platformLogging === 'debug' || this.platformLogging === 'standard';
+    return this.platformLogging === 'debugMode' || this.platformLogging === 'debug' || this.platformLogging === 'standard'
   }
 }
